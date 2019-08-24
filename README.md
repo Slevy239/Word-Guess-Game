3c6295e139dcc4bc2a7c27bed61663f98c3f44d8
Sam Levy <slevy239@gmail.com>
2019-08-24 16:25:09 -0400

    unique array

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Word-Guess-Game
 This is a game that works based on the the users key strokes.
 To start: Press any letter and the selected word will appear on screen.
+You have 15 attempts to guess the word
+Once those 15 guesses are used and the word wasn't correctly guessed, then you will lose.
--- game.js
+++ game.js
@@ -5,35 +5,47 @@ var guessedletters = [];
 var remainingguesses = 15;
 var wins = 0;
 var correctguess = [];
+var uniqueArray = [];
+console.log(random);
+// NEED TO STOP UNIQUE ARRAY FROM SHOWING LETTERS IN THE WORD
 
 
 for (var i = 0; i < random.length; i++) {   //gives randomly selected word "_" in place of it's characters
     underscored[i] = "_";
 }
 
-document.onkeyup = function start (event) {          //initiates game with key press
+document.onkeyup = function (event) {          //initiates game with key press
     if (event.keyCode > 64 && event.keyCode < 91) {
         var UserInput = event.key;
-        document.getElementById("guessedletters").textContent = [...new Set(guessedletters)];    //only allows for unique guesses
-        document.getElementById("user-guess").textContent = underscored.join(" ");  // removes commas
-        guessedletters.push(UserInput);   // displays guesses in guessedletters array
+        guessedletters.push(UserInput); // push guess into array
+
+
+
         remainingguesses--;  //removes guess
-        
-    } 
-    for (var j = 0; j < random.length; j++) {
-        if (random[j] === UserInput) {  
-            underscored[j] = UserInput;   //replaces underscore with userinput if correct
+
+        for (var j = 0; j < random.length; j++) {
+            if (random[j] === UserInput) {
+                underscored[j] = UserInput;
+                guessedletters.push(UserInput);
+                uniqueArray = guessedletters.filter(function (item, pos, self) {
+                    return self.indexOf(item) == pos;
+                });
+            }
         }
-    
+        console.log(uniqueArray);
+        document.getElementById("guessedletters").textContent = uniqueArray.join(',');
+
+        document.getElementById("user-guess").textContent = underscored.join(" ");  // removes commas
+
     }
-document.getElementById("remainingguesses").textContent = remainingguesses;  //displays guesses
-if (underscored.join("") === random) {   // win condition 
-    wins++;
-    document.getElementById("result").innerHTML = "YOU WIN!"
-    document.getElementById("wins").textContent = wins;
-}
-if (remainingguesses <= 0) { // loss
-    document.getElementById("result").innerHTML = "YOU LOSE!";
-}
+    document.getElementById("remainingguesses").textContent = remainingguesses;  //displays guesses
+    if (underscored.join("") === random) {   // win condition 
+        wins++;
+        document.getElementById("result").innerHTML = "YOU WIN!"
+        document.getElementById("wins").textContent = wins;
+    }
+    if (remainingguesses <= 0) { // loss
+        document.getElementById("result").innerHTML = "YOU LOSE!";
+    }
 };
 
