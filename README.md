11b7a46ca4bf630f9725a13abcb04a6d6ca77e0c
Sam Levy <slevy239@gmail.com>
2019-08-24 14:11:55 -0400

    fixed counter

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Word-Guess-Game
+This is a game that works based on the the users key strokes.
+To start: Press any letter and the selected word will appear on screen.
--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var possiblewords = ['javascript', 'html', 'coding', 'css'];
+var possiblewords = ['javascript', 'html', 'coding', 'css', 'jquery'];
 var random = possiblewords[Math.floor(Math.random() * possiblewords.length)];
 var underscored = [];
 var guessedletters = [];
@@ -13,29 +13,26 @@ for (var i = 0; i < random.length; i++) {   //gives randomly selected word "_" i
 
 document.onkeyup = function start (event) {          //initiates game with key press
     if (event.keyCode > 64 && event.keyCode < 91) {
-        document.getElementById("user-guess").textContent = underscored.join(" ");  // removes commas
         var UserInput = event.key;
         document.getElementById("guessedletters").textContent = [...new Set(guessedletters)];    //only allows for unique guesses
+        document.getElementById("user-guess").textContent = underscored.join(" ");  // removes commas
         guessedletters.push(UserInput);   // displays guesses in guessedletters array
-
-
-        for (var j = 0; j < random.length; j++) {
-            if (random[j] === UserInput) {  
-                underscored[j] = UserInput;   //replaces underscore with userinput if correct
-                remainingguesses--;  //removes guess
-            }
-            else {
-                remainingguesses--; // remove guess
-            }
+        remainingguesses--;  //removes guess
+        
+    } 
+    for (var j = 0; j < random.length; j++) {
+        if (random[j] === UserInput) {  
+            underscored[j] = UserInput;   //replaces underscore with userinput if correct
         }
-document.getElementById("remainingguesses").textContent = remainingguesses;  //displays guesses
+    
     }
-if (underscored.join("") === random) {   // win condition and reset
+document.getElementById("remainingguesses").textContent = remainingguesses;  //displays guesses
+if (underscored.join("") === random) {   // win condition 
     wins++;
     document.getElementById("result").innerHTML = "YOU WIN!"
-    console.log(wins)
+    document.getElementById("wins").textContent = wins;
 }
-if (remainingguesses <= 0) { // loss and reset
+if (remainingguesses <= 0) { // loss
     document.getElementById("result").innerHTML = "YOU LOSE!";
 }
 };
